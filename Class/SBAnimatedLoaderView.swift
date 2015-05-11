1377a5117f3caa09eb83c8cb87bb5f5e79bb5f42
becour_a <sacha.becourt@gmail.com>
2015-05-11 12:31:32 -0700

    Added Class + Example project

---
+++ Class/SBAnimatedLoaderView.swift
@@ -0,0 +1,74 @@
+//
+//  SBAnimatedLoaderView.swift
+//  SBLoaderView
+//
+//  Created by Sacha BECOURT on 5/11/15.
+//  Copyright (c) 2015 SB. All rights reserved.
+//
+
+import UIKit
+
+class SBAnimatedLoaderView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    init(frame: CGRect, color: UIColor, spriteName: String, numberOfSprites: Int, animationDuration: NSTimeInterval, labelString: String, labelTextColor: UIColor) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = color
+        self.alpha = 0.0
+        self.layer.cornerRadius = 10
+        
+        var soraImageView = UIImageView(frame: CGRectMake((self.frame.width - 55) / 2, 30, 50, 50))
+        soraImageView.contentMode = UIViewContentMode.Center
+        var spriteArray = NSMutableArray(capacity: 0)
+        for(var i = 0; i < numberOfSprites; i++) {
+            var image = UIImage(named: NSString(format: "%@%d", spriteName, i) as String)?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+            spriteArray.addObject(image!)
+        }
+        
+        soraImageView.animationImages = spriteArray as [AnyObject]
+        soraImageView.animationDuration = animationDuration
+        self.addSubview(soraImageView)
+        soraImageView.startAnimating()
+        
+        var loadingLabel = UILabel(frame: CGRectMake((self.frame.width - 115) / 2, self.frame.height - 40, 115, 20))
+        loadingLabel.text = labelString
+        loadingLabel.numberOfLines = 0
+        loadingLabel.textAlignment = NSTextAlignment.Center
+        loadingLabel.textColor = labelTextColor
+        
+        self.addSubview(loadingLabel)
+    }
+    
+    func show() {
+        UIView.animateWithDuration(0.3, animations: {
+            self.transform = CGAffineTransformIdentity;
+            self.alpha = 1
+        })
+    }
+    
+    func hide() {
+        UIView.animateWithDuration(0.3, animations: {
+            self.alpha = 0.0
+            self.transform = CGAffineTransformScale(self.transform, 1.5, 1.5);
+            }, completion: { (done: Bool) -> Void in
+                self.removeFromSuperview()
+        })
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
---
+++ SBLoaderViewExample/SBLoaderViewExample/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  SBLoaderViewExample
+//
+//  Created by Sacha BECOURT on 5/11/15.
+//  Copyright (c) 2015 SB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var loader = SBAnimatedLoaderView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let width = 150 as CGFloat
+        let frame = CGRectMake((self.view.frame.width - width) / 2, (self.view.frame.height - width) / 2, width, width)
+        let LoaderbackgroundColor = UIColor(red: CGFloat(22.0/255.0), green: CGFloat(158.0/255.0), blue: CGFloat(224.0/255.0), alpha: CGFloat(1.0))
+        loader = SBAnimatedLoaderView(frame: frame, color: LoaderbackgroundColor, spriteName: "sora_", numberOfSprites: 8, animationDuration: 0.8, labelString: "Loading...", labelTextColor: UIColor.whiteColor())
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func displayLoadingView(sender: AnyObject) {
+        loader.show()
+        self.view.addSubview(loader)
+    }
+    
+    @IBAction func dismissLoadingView(sender: AnyObject) {
+        loader.hide()
+    }
+
+}
+
